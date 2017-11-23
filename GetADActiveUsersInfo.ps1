4f98b08b9ef26ec98a8262ef65106776b119543a
Jose Gabriel Ortega C <jortega@j0rt3g4.com>
2017-11-22 22:42:40 -0600

    Get AD Users useful properties Information
    This script gets all AD users information for active users and exports it to a CSV file located in the same path where the scripts are being run. The script will alphabetically sort the  SamAccount Name. The information can be accessed from any domain controller or computer that have the AD tools; this is the main info:
    
    DisplayName
    SamAccountName
    Enabled
    EmailAddress
    PasswordLastSet
    LastLogonDate
    LastBadPasswordAttempt
    CannotChangePassword
    PasswordNeverExpires
    logonCount
    BadLogonCount
    This way you can just Get the CSV file into an Excel spreadsheet

---
+++ GetADActiveUsersInfo.ps1
@@ -0,0 +1,79 @@
+﻿
+$TimeStart=Get-Date
+$today = get-date -format MM-dd-yyyy
+
+function GetPasswordProperties{
+    [CmdletBinding()]
+    param($path)
+ BEGIN{
+    Write-Verbose "Importing Active Directory neccesary CMDLETS"
+    import-module activedirectory -CMDLet Get-ADUser,SetADUser
+    $TodayDate=get-date -UFormat "%Y%m%d" #"%A-%Y%m%d"
+    $Fn= "UsersInfo-$TodayDate.csv"
+    write-verbose "Getting Script Directory path"
+ }
+ PROCESS{
+    Write-Verbose "Getting information from users"  
+    $usersnames= get-aduser -filter * -Properties *
+    $AllInfoProperties=@()
+    foreach($user in $usersnames){
+    $isEnabled=$user.Enabled
+    $IDName=$user.SamAccountName
+        if($isEnabled -eq $true){
+            $AllInfoProperties+=Get-ADUser -Identity $IDName -Properties * | Select DisplayName,SamAccountName,Enabled,EmailAddress,PasswordLastSet,LastLogonDate,LastBadPasswordAttempt,CannotChangePassword,PasswordNeverExpires,logonCount,BadLogonCount|Sort-Object SamAccountName
+        }
+    }
+  
+
+    Write-Verbose "Filtering Results"
+    $pwdProp =  $AllInfoProperties|  Select  DisplayName,SamAccountName,Enabled,EmailAddress,PasswordLastSet,LastLogonDate,LastBadPasswordAttempt,CannotChangePassword,PasswordNeverExpires,logonCount,BadLogonCount
+       #Where-Object {$_.Installed -eq $true}
+    Write-Verbose "Exporting results"
+    $pwdProp | Export-csv -path "$path\$Fn" -NoTypeInformation
+    #Get-ADUser -filter * -properties Name, SamAccountName,CannotChangePassword,PasswordNeverExpires,PasswordLastSet,LastLogonDate,LastBadPasswordAttempt,BadLogonCount,logonCount | Sort-Object SamAccountName | Export-csv -path "$path\$Fn.csv"
+ }
+ END{
+   # return $pwdProp
+ }
+ 
+}
+
+function ShowTimeMS{
+  [CmdletBinding()]
+  param(
+    [Parameter(ValueFromPipeline=$True,position=0,mandatory=$true)]	[datetime]$timeStart,
+	[Parameter(ValueFromPipeline=$True,position=1,mandatory=$true)]	[datetime]$timeEnd
+  )
+  BEGIN {
+    
+  }
+  PROCESS {
+		write-Verbose "Stamping time"
+		write-Verbose  "initial time: $TimeStart"
+		write-Verbose "End time: $TimeEnd"
+		$diff=New-TimeSpan $TimeStart $TimeEnd
+		Write-verbose "Timediff= $diff"
+		$miliseconds = $diff.TotalMilliseconds
+		Write-output " Total Time in miliseconds is: $miliseconds ms"
+		
+  }
+}
+
+function Get-ScriptDirectory{
+  PROCESS{
+    $Invocation = (Get-Variable MyInvocation -Scope 1).Value
+    Split-Path $Invocation.MyCommand.Path
+  }
+}
+
+$path = Get-ScriptDirectory
+GetPasswordProperties $path
+
+#Get the end time 
+$TimeEnd=Get-Date
+
+#Show time
+ShowTimeMS $timeStart $timeEnd
+
+
+
